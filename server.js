01d22640a9b475da4730faba949ef8301f2efc11
khushboo kumari <ksingh200601@gmail.com>
2025-01-11 14:24:12 +0530

    connection to server and database done

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
-console.log('hii')
-console.log('helo')
+const AppConfig=require('./configs/app_configs')
+const express=require('express')
+const dbConfig=require('./configs/db_configs')
+const mongoose=require('mongoose')
+const app=express()
+
+//connection to DB:
+ mongoose.connect(dbConfig. DB_URL)
+ const db=mongoose.connection
+ db.on('error',()=>{
+    console.log('error while connecting to db')
+ })
+ db.once('open',()=>{
+    console.log('connected to database')
+ })
+
+//connection to server:
+app.listen(AppConfig.PORT,()=>{
+    console.log('server started on Port:',AppConfig.PORT)
+})
