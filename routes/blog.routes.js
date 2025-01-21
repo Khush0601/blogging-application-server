71fb7991ba7775360be24f58c8287d31fd55e3cf
khushboo kumari <ksingh200601@gmail.com>
2025-01-21 20:15:41 +0530

    getAllBlogs and getBlogById controller and routes made

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -1,4 +1,6 @@
 const blogController=require('../controller/blog.controller')
 module.exports=(app)=>{
     app.post('/bloggingApplication/api/v1/blog/createBlog',blogController.createBlog)
+    app.get('/bloggingApplication/api/v1/blog/getAllBlogs',blogController.getAllBlogs)
+    app.get('/bloggingApplication/api/v1/blog/:blogId',blogController.getBlogsById)
 }
