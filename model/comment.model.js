1741feaa54ffad9d4c8b6896f3a48bedbc6a3a67
khushboo kumari <ksingh200601@gmail.com>
2025-01-21 16:42:53 +0530

    commentschema made

---
+++ model/comment.model.js
@@ -0,0 +1,23 @@
+//comment schema
+
+const mongoose=require('mongoose')
+
+const commentSchema=new mongoose.Schema({
+   userId:{
+    type:mongoose.SchemaTypes.ObjectId,
+    ref:'user',
+    required:true
+   },
+   blogId:{
+    type:mongoose.SchemaTypes.ObjectId,
+    ref:'product',
+    required:true
+   },
+   commentMessage:{
+    type:String,
+    required:true,
+   },
+
+},{ timestamps: true })
+
+const Comment = mongoose.model('comment', commentSchema);
