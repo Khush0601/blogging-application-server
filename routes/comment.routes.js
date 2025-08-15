d2f4211f1901915cd7a020fa7f779a77ee46355d
khushboo kumari <ksingh200601@gmail.com>
2025-08-15 13:22:43 +0530

    updated commentRoutes

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -3,5 +3,5 @@ const verifyToken = require('../middleware/verifyToken');
 
 module.exports = app => {
   app.post('/bloggingApplication/api/v1/blog/comment/postComment', verifyToken, commentController.addComment);
-  app.get('/bloggingApplication/api/v1/blog/comment/getComment/:blogId', verifyToken, commentController.getAllCommentOfBlogId);
+  app.get('/bloggingApplication/api/v1/blog/comment/getComment/:blogId', commentController.getAllCommentOfBlogId);
 };
